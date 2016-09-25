7fdb17c5dc42a9f446a7e43900f8b4b38b49e6e3
hgdsraj <raj.axisos@gmail.com>
2016-09-25 16:18:32 -0700

    fixedfunctions for calculating grade - updated buttons

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,3 +1,23 @@
-function calculate() {
 
+function calculateMean() {
+  paragraph = document.getElementById("results");
+  paragraph.innerHTML = "HELLO WORLD";
+}
+
+function calculateWeighted() {
+  paragraph = document.getElementById("results");
+  denominators = document.getElementsByName("den");
+  numerators = document.getElementsByName("num");
+  weights = document.getElementsByName("weight");
+
+  for(i = 0; i < len; i++)
+  {
+    if(denominators[i] == 0)
+    {
+      document.alert("GRADE CANNOT BE OUT OF 0!");
+    }
+  }
+  
+  grade1 = num1/den1;
+  paragraph.innerHTML = "HELLO WORLD";
 }
