b47f8a6edbc1a2080f5049286387fbb603564435
hgdsraj <raj.axisos@gmail.com>
2016-09-25 15:52:33 -0700

    fixed bugg in calculator.js

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,8 +1,3 @@
 function calculate() {
-    var weights = [document.getElementById('largeImg').src;
-    showLargeImagePanel();
-    unselectAll();
-    setTimeout(function() {
-        document.getElementById('largeImg').src = lgSrc;
-    }, 1)
+
 }
