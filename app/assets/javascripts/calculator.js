fcd5c5af1fd06f55e1fef1920f19e8b7d53fc9eb
hgdsraj <raj.axisos@gmail.com>
2016-09-25 15:47:16 -0700

    added static pages and created full home web page - js and html and css updated

---
+++ app/assets/javascripts/calculator.js
@@ -0,0 +1,8 @@
+function calculate() {
+    var weights = [document.getElementById('largeImg').src;
+    showLargeImagePanel();
+    unselectAll();
+    setTimeout(function() {
+        document.getElementById('largeImg').src = lgSrc;
+    }, 1)
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#home'  
+  get 'home/home'
+
+  root 'home#home'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
